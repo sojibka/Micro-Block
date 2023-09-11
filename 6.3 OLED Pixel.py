4667c5f3bec2e6c971f4f61e20680ec6e0d30e63
sojibka <77024032+sojibka@users.noreply.github.com>
2023-09-11 17:34:26 +0100

    Create 6.3 OLED Pixel.py

---
+++ 6.3 OLED Pixel.py
@@ -0,0 +1,9 @@
+from machine import Pin, I2C
+from ssd1306 import SSD1306_I2C
+
+i2c=I2C(0,sda=Pin(0), scl=Pin(1), freq=400000)
+oled = SSD1306_I2C(128, 64, i2c)
+
+oled.pixel(0, 0, 1)
+oled.pixel(127, 63 , 1)
+oled.show()
