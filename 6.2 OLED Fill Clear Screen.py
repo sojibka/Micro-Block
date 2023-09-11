ca9e8f896ccc7dde23ea8037c54b040fdc3e4799
sojibka <77024032+sojibka@users.noreply.github.com>
2023-09-11 17:30:10 +0100

    Create 6.2 OLED Fill Clear Screen.py

---
+++ 6.2 OLED Fill Clear Screen.py
@@ -0,0 +1,9 @@
+from machine import Pin, I2C
+from ssd1306 import SSD1306_I2C
+
+i2c=I2C(0,sda=Pin(0), scl=Pin(1), freq=400000)
+oled = SSD1306_I2C(128, 64, i2c)
+
+oled.fill(0)                     #clear the screen
+oled.fill(1)                     #fill the screen
+oled.show()
